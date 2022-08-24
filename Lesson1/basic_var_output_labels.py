d81b3f35d6a2131685ec40ac2890ffb03f2b38df
Chompyraptor <104743441+Chompyraptor@users.noreply.github.com>
2022-08-25 09:57:07 +1200

    Add files via upload

---
+++ Lesson1/basic_var_output_labels.py
@@ -0,0 +1,20 @@
+# T R Hoshino
+# Variables, output, labels
+
+# Declaration of variables
+name = "elephant"
+numLegs = "4"
+height = "4.5"
+hasTail = "True"
+
+# Output of variables to screen
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
+
+# Output of variables with labels to screen
+print("The",name,"is one of the big five animals in Africa. ")
+print("An",name,"has",numLegs,"legs rather than 2 like primates.")
+print("The",name,"has",numLegs,"legs and stands at",height,"being one of the tallest animals.")
+print("It is",hasTail,"that it has a tail like many animals.")
