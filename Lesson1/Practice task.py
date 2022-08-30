e0770e6cc90d30c38bddb699969d28728c55d9cd
Chompyraptor <104743441+Chompyraptor@users.noreply.github.com>
2022-08-30 14:36:11 +1200

    Add files via upload

---
+++ Lesson1/Practice task.py
@@ -0,0 +1,37 @@
+# T R Hoshino
+# Variables, output, labels, data types
+
+# Variables
+name = "T R Hoshino"
+height = "176"
+address = "170 Coutts Street"
+number = "0213465234734"
+allergy = "false"
+
+# A summary of printout
+# Output = print()
+print (name)
+print (height)
+print (address)
+print (number)
+print (allergy)
+
+# Clarify the problem
+# Name (var)
+firstName = "T"
+middleName = "R"
+lastName = "Hoshino"
+
+# Height - expressed in centimetres
+unitHeight = "cm"
+heightWithUnit = "176 cm"
+# Address
+# Street number, street name, suburb, city
+numAddress = "170"
+streetAddress = "Coutts"
+suburbAddress = "Rongotai"
+cityAddress = "Wellington"
+
+
+# Phone number - landline or mobile
+# Stored as a string - no calc
