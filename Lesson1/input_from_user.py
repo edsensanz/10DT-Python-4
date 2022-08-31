38dce7ace0b20afaf4b83a289d15971ae0576e65
Chompyraptor <104743441+Chompyraptor@users.noreply.github.com>
2022-09-01 10:04:34 +1200

    Add files via upload

---
+++ Lesson1/input_from_user.py
@@ -0,0 +1,37 @@
+# T R Hoshino
+# User input
+
+#name = input("Enter your first name: ")
+
+# print("Hello,",name,", I hope you have a good day.")
+
+# data types for input
+
+# height = input("How tall are you? : ")
+# weight = input("What is your weight: ")
+# favColour = input("Enter your favourite colour? : ")
+# havePet = input("Do you have a pet? : ")
+# favPet = input("Enter your favourte pet: ")
+
+
+# taking input from the user as integer
+#num = int(input("Enter a number: "))
+#add = num + 1
+
+# output
+# print(add)
+
+# taking input from the user as float
+#num = float(input("Enter a number: "))
+#add = num + 1
+
+# output
+# print add
+
+# taking input from the user as list
+# li = list(input("Enter a number of 5 digits: "))
+# print(li)
+
+# taking input from the user as tuple
+# num = tuple(input("Enter a number: "))
+# print(num)
